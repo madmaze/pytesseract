ea0aa38133ece50aef40675fbd010e774bd70d93
jbochi <jbochi@gmail.com>
2010-10-27 21:09:50 -0200

    Added option to return boxes

--- tesseract.py
+++ tesseract.py
@@ -52,7 +52,7 @@ import os
 
 __all__ = ['image_to_string']
 
-def run_tesseract(input_filename, output_filename_base, lang=None):
+def run_tesseract(input_filename, output_filename_base, lang=None, boxes=False):
     '''
     runs the command:
         `tesseract_cmd` `input_filename` `output_filename_base`
@@ -62,6 +62,10 @@ def run_tesseract(input_filename, output_filename_base, lang=None):
     '''
 
     command = [tesseract_cmd, input_filename, output_filename_base]
+    
+    if boxes:
+        command += ['batch.nochop', 'makebox']
+    
     if lang is not None:
         command += ['-l', lang]
 
@@ -106,7 +110,7 @@ class TesseractError(Exception):
         self.message = message
         self.args = (status, message)
 
-def image_to_string(image, lang=None):
+def image_to_string(image, lang=None, boxes=False):
     '''
     Runs tesseract on the specified image. First, the image is written to disk,
     and then the tesseract command is run on the image. Resseract's result is
@@ -119,7 +123,10 @@ def image_to_string(image, lang=None):
     output_file_name = '%s.txt' % output_file_name_base
     try:
         image.save(input_file_name)
-        status, error_string = run_tesseract(input_file_name, output_file_name_base, lang=lang)
+        status, error_string = run_tesseract(input_file_name,
+                                             output_file_name_base,
+                                             lang=lang,
+                                             boxes=boxes)
         if status:
             errors = get_errors(error_string)
             raise TesseractError(status, errors)
