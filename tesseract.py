3c4f139dc9f0f696eefcb5624ce316278a9a85fd
Jerome Flesch <jflesch@gmail.com>
2011-11-11 22:26:37 +0100

    Fix TesseractError

--- tesseract.py
+++ tesseract.py
@@ -118,6 +118,7 @@ def tempnam():
 
 class TesseractError(Exception):
     def __init__(self, status, message):
+        Exception.__init__(self, message)
         self.status = status
         self.message = message
         self.args = (status, message)
