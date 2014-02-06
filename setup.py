f0b2bea573da791e8016a103be3331bab4289cda
madmaze <matthias.a.lee@gmail.com>
2014-02-05 23:22:00 -0500

    updated README and setup.py to indicate maintainer changes

--- setup.py
+++ setup.py
@@ -8,12 +8,14 @@ def read(fname):
 setup(
     name = "pytesseract",
     version = "0.1",
-    author = "Juarez Bochi",
+    author = "Samuel Hoffstaetter",
     author_email="",
+    maintainer = "Matthias Lee",
+    maintainer_email = "pytesseract@madmaze.net",
     description = ("Python-tesseract is a python wrapper for google's Tesseract-OCR "),
     license = "GPLv3",
     keywords = "python-tesseract OCR Python",
-    url = "https://github.com/jbochi/python-tesseract",
+    url = "https://github.com/madmaze/python-tesseract",
     packages=['pytesseract'],
     long_description=read('README'),
     package_data = {'pytesseract': ['*.png','*.jpg']}
