0069773f46b5c05a38f2600ba296b7b050a4fa0e
Simeon Visser <simeon@rangespan.com>
2014-04-07 15:29:43 +0100

    Fixed ImportError when using Pillow

--- pytesseract/pytesseract.py
+++ pytesseract/pytesseract.py
@@ -44,7 +44,10 @@ http://wiki.github.com/hoffstaetter/python-tesseract
 # CHANGE THIS IF TESSERACT IS NOT IN YOUR PATH, OR IS NAMED DIFFERENTLY
 tesseract_cmd = 'tesseract'
 
-import Image
+try:
+    import Image
+except ImportError:
+    from PIL import Image
 import StringIO
 import subprocess
 import sys
