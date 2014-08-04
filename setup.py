41c3e30ee1e1623b837f06afb8a3906d1bedba4d
madmaze <matthias.a.lee@gmail.com>
2014-08-04 18:25:20 -0400

    removed reading of readme.md from setup.py as it caused issues when installing through pip

--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
 import os
 from setuptools import setup
 
-# Function for reading readme.
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
     name = "pytesseract",
-    version = "0.1",
+    version = "0.1.3",
     author = "Samuel Hoffstaetter",
     author_email="",
     maintainer = "Matthias Lee",
     maintainer_email = "pytesseract@madmaze.net",
-    description = ("Python-tesseract is a python wrapper for google's Tesseract-OCR "),
+    description = ("Python-tesseract is a python wrapper for google's Tesseract-OCR"),
     license = "GPLv3",
     keywords = "python-tesseract OCR Python",
     url = "https://github.com/madmaze/python-tesseract",
     packages=['pytesseract'],
-    long_description=read('README.md'),
     package_data = {'pytesseract': ['*.png','*.jpg']}
 )
