5223947a1b1a1e86f6b7128bd36c0461abd31c6f
madmaze <matthias.a.lee@gmail.com>
2014-08-14 09:46:15 -0400

    added config from pull request #5

--- src/pytesseract.py
+++ src/pytesseract.py
@@ -52,7 +52,7 @@ import os
 
 __all__ = ['image_to_string']
 
-def run_tesseract(input_filename, output_filename_base, lang=None, boxes=False):
+def run_tesseract(input_filename, output_filename_base, lang=None, boxes=False, config=None):
     '''
     runs the command:
         `tesseract_cmd` `input_filename` `output_filename_base`
@@ -67,6 +67,9 @@ def run_tesseract(input_filename, output_filename_base, lang=None, boxes=False):
 
     if boxes:
         command += ['batch.nochop', 'makebox']
+        
+    if config:
+        command += [config]
     
     proc = subprocess.Popen(command,
             stderr=subprocess.PIPE)
@@ -109,11 +112,18 @@ class TesseractError(Exception):
         self.message = message
         self.args = (status, message)
 
-def image_to_string(image, lang=None, boxes=False):
+def image_to_string(image, lang=None, boxes=False, config=None):
     '''
     Runs tesseract on the specified image. First, the image is written to disk,
     and then the tesseract command is run on the image. Resseract's result is
     read, and the temporary files are erased.
+    
+    also supports boxes and config.
+    
+    if boxes=True
+        "batch.nochop makebox" gets added to the tesseract call
+    if config is set, the config gets appended to the command.
+        ex: config="-psm 6"
 
     '''
 
@@ -128,7 +138,8 @@ def image_to_string(image, lang=None, boxes=False):
         status, error_string = run_tesseract(input_file_name,
                                              output_file_name_base,
                                              lang=lang,
-                                             boxes=boxes)
+                                             boxes=boxes,
+                                             config=config)
         if status:
             errors = get_errors(error_string)
             raise TesseractError(status, errors)
