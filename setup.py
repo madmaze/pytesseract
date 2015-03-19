02b805289f4c6ff6ecf644b0d0f02b045e533369
madmaze <matthias.a.lee@gmail.com>
2015-03-19 14:34:11 -0400

    fixed the version bump, also now including README.md as long_description in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 import os
 from setuptools import setup
 
+
+longDesc = ""
+if os.path.exits("README.md"):
+	longDesc = open("README.md").read().strip()
+
 setup(
     name = "pytesseract",
-    version = "0.2",
+    version = "0.1.6",
     author = "Samuel Hoffstaetter",
     author_email="",
     maintainer = "Matthias Lee",
     maintainer_email = "pytesseract@madmaze.net",
     description = ("Python-tesseract is a python wrapper for google's Tesseract-OCR"),
+    long_description = longDesc,
     license = "GPLv3",
     keywords = "python-tesseract OCR Python",
     url = "https://github.com/madmaze/python-tesseract",
