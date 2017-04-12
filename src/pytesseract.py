17019732272836ed154417cb520b6128ed06bd4a
John Francis <jfswitz@gmail.com>
2017-04-12 20:22:50 +0200

    fix python 3 byte string bug (#48)

--- src/pytesseract.py
+++ src/pytesseract.py
@@ -1,46 +1,51 @@
 #!/usr/bin/env python
+
 '''
 Python-tesseract. For more information: https://github.com/madmaze/pytesseract
 
 '''
 
-# CHANGE THIS IF TESSERACT IS NOT IN YOUR PATH, OR IS NAMED DIFFERENTLY
-tesseract_cmd = 'tesseract'
-
 try:
     import Image
 except ImportError:
     from PIL import Image
-import subprocess
-import sys
-import tempfile
+
 import os
+import sys
+import subprocess
+import tempfile
 import shlex
+
+
+# CHANGE THIS IF TESSERACT IS NOT IN YOUR PATH, OR IS NAMED DIFFERENTLY
+tesseract_cmd = 'tesseract'
 
 __all__ = ['image_to_string']
 
-def run_tesseract(input_filename, output_filename_base, lang=None, boxes=False, config=None):
+
+def run_tesseract(input_filename, output_filename_base, lang=None, boxes=False,
+                  config=None):
     '''
     runs the command:
         `tesseract_cmd` `input_filename` `output_filename_base`
-    
+
     returns the exit status of tesseract, as well as tesseract's stderr output
 
     '''
     command = [tesseract_cmd, input_filename, output_filename_base]
-    
+
     if lang is not None:
         command += ['-l', lang]
 
     if boxes:
         command += ['batch.nochop', 'makebox']
-        
+
     if config:
         command += shlex.split(config)
-    
-    proc = subprocess.Popen(command,
-            stderr=subprocess.PIPE)
+
+    proc = subprocess.Popen(command, stderr=subprocess.PIPE)
     return (proc.wait(), proc.stderr.read())
+
 
 def cleanup(filename):
     ''' tries to remove the given filename. Ignores non-existent files '''
@@ -49,23 +54,27 @@ def cleanup(filename):
     except OSError:
         pass
 
+
 def get_errors(error_string):
     '''
     returns all lines in the error_string that start with the string "error"
 
     '''
 
+    error_string = error_string.decode('utf-8')
     lines = error_string.splitlines()
-    error_lines = tuple(line for line in lines if line.find('Error') >= 0)
+    error_lines = tuple(line for line in lines if line.find(u'Error') >= 0)
     if len(error_lines) > 0:
-        return '\n'.join(error_lines)
+        return u'\n'.join(error_lines)
     else:
         return error_string.strip()
+
 
 def tempnam():
     ''' returns a temporary file-name '''
     tmpfile = tempfile.NamedTemporaryFile(prefix="tess_")
     return tmpfile.name
+
 
 class TesseractError(Exception):
     def __init__(self, status, message):
@@ -73,19 +82,20 @@ class TesseractError(Exception):
         self.message = message
         self.args = (status, message)
 
+
 def image_to_string(image, lang=None, boxes=False, config=None):
     '''
     Runs tesseract on the specified image. First, the image is written to disk,
     and then the tesseract command is run on the image. Tesseract's result is
     read, and the temporary files are erased.
-    
-    also supports boxes and config.
-    
+
+    Also supports boxes and config:
+
     if boxes=True
         "batch.nochop makebox" gets added to the tesseract call
+
     if config is set, the config gets appended to the command.
         ex: config="-psm 6"
-
     '''
 
     if len(image.split()) == 4:
@@ -93,7 +103,7 @@ def image_to_string(image, lang=None, boxes=False, config=None):
         # Kind of a hack, should fix in the future some time.
         r, g, b, a = image.split()
         image = Image.merge("RGB", (r, g, b))
-    
+
     input_file_name = '%s.bmp' % tempnam()
     output_file_name_base = tempnam()
     if not boxes:
@@ -119,6 +129,7 @@ def image_to_string(image, lang=None, boxes=False, config=None):
         cleanup(input_file_name)
         cleanup(output_file_name)
 
+
 def main():
     if len(sys.argv) == 2:
         filename = sys.argv[1]
@@ -143,8 +154,9 @@ def main():
             exit(1)
         print(image_to_string(image, lang=lang))
     else:
-        sys.stderr.write('Usage: python pytesseract.py [-l language] input_file\n')
+        sys.stderr.write('Usage: python pytesseract.py [-l lang] input_file\n')
         exit(2)
+
 
 if __name__ == '__main__':
     main()
