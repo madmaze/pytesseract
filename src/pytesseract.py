13a860e96214b69c14c8beaef040cff7ed2cb7ba
Josh Reid <jsreid13@gmail.com>
2017-12-13 23:48:05 -0500

    Improved method to discard alpha channel
    Changed the current method that uses split with the faster convert
    method within PIL.

--- src/pytesseract.py
+++ src/pytesseract.py
@@ -100,7 +100,7 @@ def image_to_string(image, lang=None, boxes=False, config=None, nice=0):
     '''
 
     if len(image.split()) == 4:
-        time0 = time.clock()
+        # In case we have 4 channels, lets discard the Alpha.
         image.convert('RGB')
 
     temp_name = tempfile.mktemp(prefix='tess_')
