a52b10f9e595cd3b3c14facbdc320656deb85b51
REMitchell <Ryan.E.Mitchell@gmail.com>
2018-01-14 10:47:57 -0500

    Support for different output types
    Fixes #87

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,9 @@ try:
     from pytesseract import image_to_string
     from pytesseract import image_to_data
     from pytesseract import image_to_boxes
+    from pytesseract import Output
 except ImportError:
     from pytesseract.pytesseract import image_to_string
+    from pytesseract.pytesseract import image_to_data
+    from pytesseract.pytesseract import image_to_boxes
+    from pytesseract.pytesseract import Output
