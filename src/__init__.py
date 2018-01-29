1f4dd22f174997a6994399878d793f81f2b47a33
int3l <int3l@users.noreply.github.com>
2018-01-29 20:03:42 +0200

    Refactor duplicated imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,7 @@
-try:
-    from pytesseract import image_to_string
-    from pytesseract import image_to_data
-    from pytesseract import image_to_boxes
-    from pytesseract import Output
-except ImportError:
-    from pytesseract.pytesseract import image_to_string
-    from pytesseract.pytesseract import image_to_data
-    from pytesseract.pytesseract import image_to_boxes
-    from pytesseract.pytesseract import Output
+from .pytesseract import (
+    image_to_string,
+    image_to_data,
+    image_to_boxes,
+    TesseractError,
+    Output
+)
