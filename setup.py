ae8191b16dba38df65f96aa8f3949cd2808d1284
int3l <int3l@users.noreply.github.com>
2018-02-16 23:03:04 +0200

    Bump version to 0.2.1
    * Documentation and link ref fixes
    * Suppression for the Windows console window

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ PACKAGE_DIR = 'src'
 
 setup(
     name=PACKAGE_NAME,
-    version='0.2.0',
+    version='0.2.1',
     author='Samuel Hoffstaetter',
     author_email='samuel@hoffstaetter.com',
     maintainer='Matthias Lee',
