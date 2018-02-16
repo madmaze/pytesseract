2802cc642c24dcd863506c6116f17e15c60792a2
2press <pres.sure@ymail.com>
2018-02-16 13:43:39 +0100

    Update pytesseract.py
    Removed comments and the redundant argument.

--- src/pytesseract.py
+++ src/pytesseract.py
@@ -87,39 +87,21 @@ def save_image(image):
     return temp_name, input_file_name
 
 def subprocess_args(include_stdout=True):
-    # The following is true only on Windows.
+    # See https://github.com/pyinstaller/pyinstaller/wiki/Recipe-subprocess
+    # for reference and comments.
     if hasattr(subprocess, 'STARTUPINFO'):
-        # On Windows, subprocess calls will pop up a command window by default
-        # when run from Pyinstaller with the ``--noconsole`` option. Avoid this
-        # distraction.
         si = subprocess.STARTUPINFO()
         si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-        # Windows doesn't search the path by default. Pass it an environment so
-        # it will.
         env = os.environ
     else:
         si = None
         env = None
 
-    # ``subprocess.check_output`` doesn't allow specifying ``stdout``::
-    #
-    #   Traceback (most recent call last):
-    #     File "test_subprocess.py", line 58, in <module>
-    #       **subprocess_args(stdout=None))
-    #     File "C:\Python27\lib\subprocess.py", line 567, in check_output
-    #       raise ValueError('stdout argument not allowed, it will be overridden.')
-    #   ValueError: stdout argument not allowed, it will be overridden.
-    #
-    # So, add it only if it's needed.
     if include_stdout:
         ret = {'stdout': subprocess.PIPE}
     else:
         ret = {}
 
-    # On Windows, running this from the binary produced by Pyinstaller
-    # with the ``--noconsole`` option requires redirecting everything
-    # (stdin, stdout, stderr) to avoid an OSError exception
-    # "[Error 6] the handle is invalid."
     ret.update({'stdin': subprocess.PIPE,
                 'stderr': subprocess.PIPE,
                 'startupinfo': si,
@@ -147,7 +129,7 @@ def run_tesseract(input_filename,
     if extension != 'box':
         command.append(extension)
 
-    proc = subprocess.Popen(command, **subprocess_args(True))
+    proc = subprocess.Popen(command, **subprocess_args())
     status_code, error_string = proc.wait(), proc.stderr.read()
     proc.stderr.close()
 
