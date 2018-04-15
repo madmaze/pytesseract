61198d72f7c7ae8ea0ce2b48be796c8158df62e8
Emilio Cecchini <cecchinator@gmail.com>
2018-04-15 16:35:26 +0200

    image_to_osd function (#114)
    * Added image_to_osd function to run Tesseract with --psm 0 parameter
    
    * Added image_to_osd in __init__
    
    * lines as generator and extracted OSD_KEYS constant

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from .pytesseract import (
     image_to_string,
     image_to_data,
     image_to_boxes,
+    image_to_osd,
     TesseractError,
     Output
 )
--- src/pytesseract.py
+++ src/pytesseract.py
@@ -2,7 +2,6 @@
 
 '''
 Python-tesseract. For more information: https://github.com/madmaze/pytesseract
-
 '''
 
 try:
@@ -25,6 +24,14 @@ if numpy_installed:
 # CHANGE THIS IF TESSERACT IS NOT IN YOUR PATH, OR IS NAMED DIFFERENTLY
 tesseract_cmd = 'tesseract'
 RGB_MODE = 'RGB'
+OSD_KEYS = {
+    'Page number': ('page_num', int),
+    'Orientation in degrees': ('orientation', int),
+    'Rotate': ('rotate', int),
+    'Orientation confidence': ('orientation_conf', float),
+    'Script': ('script', str),
+    'Script confidence': ('script_conf', float)
+}
 
 
 class Output:
@@ -192,6 +199,11 @@ def file_to_dict(tsv, cell_delimiter, str_col_idx):
     return result
 
 
+def osd_to_dict(osd):
+    lines = (line.split(': ') for line in osd.split('\n'))
+    return {OSD_KEYS[k][0]: OSD_KEYS[k][1](v) for k, v in lines}
+
+
 def image_to_string(image,
                     lang=None,
                     config='',
@@ -252,6 +264,25 @@ def image_to_data(image,
         return run_and_get_output(image, 'tsv', lang, config, nice, True)
 
     return run_and_get_output(image, 'tsv', lang, config, nice)
+
+
+def image_to_osd(image,
+                 lang=None,
+                 config='',
+                 nice=0,
+                 output_type=Output.STRING):
+    '''
+    Returns string containing the orientation and script detection (OSD)
+    '''
+    config += ' --psm 0'
+
+    if output_type == Output.DICT:
+        return osd_to_dict(
+            run_and_get_output(image, 'osd', lang, config, nice))
+    elif output_type == Output.BYTES:
+        return run_and_get_output(image, 'osd', lang, config, nice, True)
+
+    return run_and_get_output(image, 'osd', lang, config, nice)
 
 
 def main():
