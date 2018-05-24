1b707b407d7f99a83605bbbb1c492a33427d765c
int3l <int3l@users.noreply.github.com>
2018-05-24 15:01:49 +0300

    Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ PACKAGE_DIR = 'src'
 
 setup(
     name=PACKAGE_NAME,
-    version='0.2.1',
+    version='0.2.2',
     author='Samuel Hoffstaetter',
     author_email='samuel@hoffstaetter.com',
     maintainer='Matthias Lee',
