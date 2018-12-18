25a9d38649f6d9f907f9c6750cab03d699d0b340
int3l <int3l@users.noreply.github.com>
2018-12-18 10:22:52 +0200

    only python 2.7 support classifier

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     },
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
     ]
 )
