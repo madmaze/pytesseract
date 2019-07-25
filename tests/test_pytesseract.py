0ae011a2b9e838fb8178f936b757d106dbb5bae1
Darius Morawiec <nok@users.noreply.github.com>
2019-07-25 23:59:59 +0200

    feature/expand_testing: Refactor and use fixtures for common test files Add tests to validate the output of the method `image_to_data`

--- tests/test_pytesseract.py
+++ tests/test_pytesseract.py
@@ -1,25 +1,82 @@
 import os.path
+from sys import version_info as PYTHON_VERSION
 
 import pytest
 
-from pytesseract import image_to_string
+from pytesseract import image_to_string, Output, get_tesseract_version, \
+    image_to_data
 
+try:
+    import pandas
+except ImportError:
+    pandas = None
+
+
+TESSERACT_VERSION = tuple(get_tesseract_version().version)  # to skip tests
 DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 
 
-@pytest.mark.pytesseract
-def test_quick_brown_dog_image():
-    test_file = os.path.join(DATA_DIR, 'test.png')
+pytestmark = pytest.mark.pytesseract  # used marker for the module
 
+
+@pytest.fixture(scope='session')
+def test_file():
+    return os.path.join(DATA_DIR, 'test.png')
+
+
+def test_quick_brown_dog_image(test_file):
     # Don't perform assertion against full string in case the version
     # of tesseract installed doesn't catch it all. This test is testing
     # that pytesseract command line program is called correctly.
     assert 'The quick brown dog' in image_to_string(test_file, 'eng')
 
 
-@pytest.mark.pytesseract
 @pytest.mark.lang_fra
 def test_european_image():
     test_file = os.path.join(DATA_DIR, 'test-european.jpg')
-
     assert 'La volpe marrone' in image_to_string(test_file, 'fra')
+
+
+@pytest.mark.skipif(
+    TESSERACT_VERSION[:2] < (3, 5),
+    reason='requires tesseract >= 3.05'
+)
+@pytest.mark.skipif(
+    pandas is None,
+    reason='requires pandas'
+)
+def test_pandas_output(test_file):
+    """Test and compare the type and meta information of the result."""
+    result = image_to_data(test_file, output_type=Output.DATAFRAME)
+    assert isinstance(result, pandas.DataFrame)
+    expected_columns = ['level', 'page_num', 'block_num', 'par_num',
+                        'line_num', 'word_num', 'left', 'top', 'width',
+                        'height', 'conf', 'text']
+    assert bool(set(result.columns).intersection(expected_columns))
+
+
+@pytest.mark.skipif(
+    TESSERACT_VERSION[:2] < (3, 5),
+    reason='requires tesseract >= 3.05'
+)
+@pytest.mark.parametrize('output', [
+    Output.BYTES,
+    Output.DICT,
+    Output.STRING,
+], ids=[
+    'bytes',
+    'dict',
+    'string',
+])
+def test_common_output(test_file, output):
+    """Test and compare the type of the result."""
+    result = image_to_data(test_file, output_type=output)
+    if output is Output.BYTES:
+        assert isinstance(result, bytes)
+    elif output is Output.DICT:
+        assert isinstance(result, dict)
+    elif output is Output.STRING:
+        if PYTHON_VERSION[:1] < (3, ):
+            assert isinstance(result, unicode)
+        else:
+            assert isinstance(result, str)
