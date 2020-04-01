e96253ea17a83b31bf3d2e5a118c2180174d1105
int3l <int3l@users.noreply.github.com>
2020-04-01 03:15:10 +0300

    Add python 3.8 classifier to setup.py [skip ci]

--- setup.py
+++ setup.py
@@ -42,5 +42,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
