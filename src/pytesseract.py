6d4e78c550704776d90640125096bf11e60b0a3c
Steve (Gadget) Barnes <gadgetsteve@hotmail.com>
2020-05-26 17:08:43 +0100

    Add README hit to error message
    As per discussion on #272 & #273.

--- src/pytesseract.py
+++ src/pytesseract.py
@@ -78,7 +78,8 @@ class TesseractError(RuntimeError):
 class TesseractNotFoundError(EnvironmentError):
     def __init__(self):
         super(TesseractNotFoundError, self).__init__(
-            tesseract_cmd + " is not installed or it's not in your PATH",
+            tesseract_cmd + " is not installed or it's not in your PATH." +
+            " See README for more information.",
         )
 
 
