b4d7ced225b36484a98812abfcc5ac9b7fd4f401
eivtho <eivtho@arkivverket.no>
2020-08-03 15:39:02 +0200

    fix alphabetic ordering in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -4,11 +4,11 @@ from .pytesseract import (  # noqa: F401
     TesseractNotFoundError,
     TSVNotSupported,
     get_tesseract_version,
+    image_to_alto_xml,
     image_to_boxes,
     image_to_data,
     image_to_osd,
     image_to_pdf_or_hocr,
-    image_to_alto_xml,
     image_to_string,
     run_and_get_output,
 )
