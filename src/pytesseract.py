madmaze/pytesseract

commit 6d5a35213d7e8dcf836e7107fa86ac62265cfa05

Don't strip output data from tesseract (#269)

src/pytesseract.py
@@ -272,7 +272,7 @@ def run_and_get_output(
         with open(filename, 'rb') as output_file:
             if return_bytes:
                 return output_file.read()
-            return output_file.read().decode('utf-8').strip()
+            return output_file.read().decode('utf-8')
 
 
 def file_to_dict(tsv, cell_delimiter, str_col_idx):

commit c889d6e8809624b25a45feac9f7c66106425fa5d

Fix TSV parsing - white space at start/end of file
Fixes #290 - partial regression from 6d5a35213d7e8dcf836e7107fa86ac62265cfa05

src/pytesseract.py
@@ -280,7 +280,7 @@ def run_and_get_output(
 
 def file_to_dict(tsv, cell_delimiter, str_col_idx):
     result = {}
-    rows = [row.split(cell_delimiter) for row in tsv.split('\n')]
+    rows = [row.split(cell_delimiter) for row in tsv.strip().split('\n')]
     if not rows:
         return result
 