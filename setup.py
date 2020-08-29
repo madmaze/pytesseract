110ad667c0404652666ac43b7de9583a568af65f
int3l <int3l@users.noreply.github.com>
2020-08-29 22:33:43 +0300

    Bump version to 0.3.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ PACKAGE_DIR = 'src'
 
 setup(
     name=PACKAGE_NAME,
-    version='0.3.5',
+    version='0.3.6',
     author='Samuel Hoffstaetter',
     author_email='samuel@hoffstaetter.com',
     maintainer='Matthias Lee',
